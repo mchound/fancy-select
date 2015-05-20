03f3c89d9e9af68bea0ad26a3b545f8ffb45fb9c
tobias <tobias@welcom.se>
2015-05-20 09:21:09 +0200

    Styling for viewports

--- javascripts/src/components/FancySelect.react.jsx
+++ javascripts/src/components/FancySelect.react.jsx
@@ -11,7 +11,24 @@ module.exports = React.createClass({
 				{value: 'no', text: 'Norway'},
 				{value: 'dk', text: 'Denmark'},
 				{value: 'fi', text: 'Finland'},
-				{value: 'ic', text: 'Iceland'}
+				{value: 'ic', text: 'Iceland'},
+				{value: 'ge', text: 'Germany'},
+				{value: 'en', text: 'England'},
+				{value: 'sc', text: 'Scotland'},
+				{value: 'fr', text: 'France'},
+				{value: 'po', text: 'Poland'},
+				{value: 'ne', text: 'Netherlands'},
+				{value: 'be', text: 'Belgium'},
+				{value: 'it', text: 'Italy'},
+				{value: 'au', text: 'Austria'},
+				{value: 'sw', text: 'Switzerland'},
+				{value: 'sp', text: 'Spain'},
+				{value: 'por', text: 'Portugal'},
+				{value: 'gr', text: 'Greece'},
+				{value: 'sl', text: 'Slovakia'},
+				{value: 'es', text: 'Estonia'},
+				{value: 'lv', text: 'Latvia'},
+				{value: 'li', text: 'Lithuania'}
 			],
 			selected: null,
 			filter: ''
@@ -21,7 +38,7 @@ module.exports = React.createClass({
 
 	componentDidMount: function(){
 		var node = this.refs.search.getDOMNode();
-		node.focus();
+		setTimeout(function(){ node.focus(); }, 10);
 	},
 
 	render: function(){
@@ -32,10 +49,16 @@ module.exports = React.createClass({
 			<div data-am-fancyselect>
 
 				<div className="select" onClick={this._onSelectClick}>
-					Choose Country
+					{!!this.state.selected ? this.state.selected.text : 'Choose Country'}
 				</div>
 
 				<div className={optionsClassName}>
+
+					<div className="backdrop" onClick={this._close}></div>
+
+					<div className="top-bar">
+						<button className="btn-close" onClick={this._close}></button><span className="title">Country Select</span>
+					</div>
 
 					<div className="search">
 						<input type="text" placeholder="Search country" ref="search" onKeyUp={this._onSearch} />
@@ -74,7 +97,7 @@ module.exports = React.createClass({
 	_onItemClick: function(item){
 		this.setState({
 			filter: '',
-			selected: item,
+			selected: !!this.state.selected && item.value === this.state.selected.value ? null : item,
 			showOptions: false
 		});
 		var node = this.refs.search.getDOMNode();
@@ -83,6 +106,10 @@ module.exports = React.createClass({
 
 	_onSelectClick: function(e){
 		this.setState({showOptions: true});
+	},
+
+	_close: function(){
+		this.setState({showOptions: false});	
 	}
 
 
